cd3b0bd60f5319559de1eb68d4c969a166e2d5c4
epeios-q37 <epeios-q37@users.noreply.github.com>
2017-06-07 18:56:19 +0200

    07/06/2017 18:56:19

--- test.js
+++ test.js
@@ -1,5 +1,9 @@
+console.log( "***** TEST BEGIN *****" );
+
 const njsq = require( './njsq.js');
 
 console.log( njsq.wrapperInfo() );
 console.log( njsq.addonInfo() );
+
+console.log( "***** TEST END *****" );
 
